317ac43097791c754cc0015a461f706cff755d6c
Alexanderius <alexander.krylkov@gmail.com>
2015-03-15 10:10:48 +0600

    Downgrade initial

--- SailPlayer/src/Audio/AudioPlayerBase.cpp
+++ SailPlayer/src/Audio/AudioPlayerBase.cpp
@@ -1,5 +1,7 @@
 #include "AudioPlayerBase.hpp"
 
+#include <QDebug>
+
 namespace Audio
 {
 	int AudioPlayerBase::EqualizerBandsNumber = 18;
@@ -94,7 +96,7 @@ namespace Audio
 	{
 		gint64 value;
 
-		if(gst_element_query_position(_pipeline, _gstTimeFormat, &value))
+		if(gst_element_query_position(_pipeline, &_gstTimeFormat, &value))
 			return value;
 
 		return -1;
@@ -109,7 +111,7 @@ namespace Audio
 	{
 		gint64 value;
 
-		if(gst_element_query_duration(_pipeline, _gstTimeFormat, &value))
+		if(gst_element_query_duration(_pipeline, &_gstTimeFormat, &value))
 			return value;
 
 		return -1;
@@ -117,7 +119,7 @@ namespace Audio
 
 	bool AudioPlayerBase::Init()
 	{
-		_pipeline = gst_parse_launch("playbin", NULL);
+		_pipeline = gst_parse_launch("playbin2", NULL);
 		_equalizer = gst_element_factory_make ("equalizer-nbands", NULL);
 		_sink = gst_element_factory_make("autoaudiosink", NULL);
 
@@ -168,47 +170,47 @@ namespace Audio
 	void AudioPlayerBase::SetEqualizerData()
 	{
 		gint i;
-		GObject *band;
+		GstObject *band;
 
 		// TODO
 		GstEqualizerBandState equalizerData[] = {
-			{ 55.0,   22.0, 0.0},
-			{ 77.0,   33.0, 0.0},
-			{110.0,   46.0, 0.0},
-			{156.0,   64.0, 0.0},
-			{220.0,   91.0, 0.0},
-			{311.0,   129.0, 0.0},
-			{440.0,   182.0, 0.0},
-			{622.0,   258.0, 0.0},
-			{880.0,   320.0, 0.0},
-			{1200.0,   600.0, 0.0},
-			{1800.0,   700.0, 0.0},
-			{2500.0,   1000.0, 0.0},
-			{3500.0,   1500.0, 0.0},
-			{5000.0,   2000.0, 0.0},
-			{7000.0,   3000.0, 0.0},
-			{10000.0,  4000.0, 0.0},
-			{14000.0,  6000.0, 0.0},
-			{20000.0,  6000.0, 0.0},
-
 //			{ 55.0,   22.0, 0.0},
-//			{ 77.0,   33.0, 1.0},
+//			{ 77.0,   33.0, 0.0},
 //			{110.0,   46.0, 0.0},
 //			{156.0,   64.0, 0.0},
-//			{220.0,   91.0, -1.0},
-//			{311.0,   129.0, -2.0},
-//			{440.0,   182.0, -3.0},
-//			{622.0,   258.0, -2.0},
-//			{880.0,   320.0, -1.0},
+//			{220.0,   91.0, 0.0},
+//			{311.0,   129.0, 0.0},
+//			{440.0,   182.0, 0.0},
+//			{622.0,   258.0, 0.0},
+//			{880.0,   320.0, 0.0},
 //			{1200.0,   600.0, 0.0},
 //			{1800.0,   700.0, 0.0},
 //			{2500.0,   1000.0, 0.0},
 //			{3500.0,   1500.0, 0.0},
 //			{5000.0,   2000.0, 0.0},
 //			{7000.0,   3000.0, 0.0},
-//			{10000.0,  4000.0, 1.0},
-//			{14000.0,  6000.0, 1.0},
-//			{20000.0,  6000.0, 1.0},
+//			{10000.0,  4000.0, 0.0},
+//			{14000.0,  6000.0, 0.0},
+//			{20000.0,  6000.0, 0.0},
+
+			{ 55.0,   22.0, 0.0},
+			{ 77.0,   33.0, 1.0},
+			{110.0,   46.0, 0.0},
+			{156.0,   64.0, 0.0},
+			{220.0,   91.0, -1.0},
+			{311.0,   129.0, -2.0},
+			{440.0,   182.0, -3.0},
+			{622.0,   258.0, -2.0},
+			{880.0,   320.0, -1.0},
+			{1200.0,   600.0, 0.0},
+			{1800.0,   700.0, 0.0},
+			{2500.0,   1000.0, 0.0},
+			{3500.0,   1500.0, 0.0},
+			{5000.0,   2000.0, 0.0},
+			{7000.0,   3000.0, 0.0},
+			{10000.0,  4000.0, 1.0},
+			{14000.0,  6000.0, 1.0},
+			{20000.0,  6000.0, 1.0},
 		};
 
 		for (i = 0; i < EqualizerBandsNumber; i++)
@@ -238,11 +240,12 @@ namespace Audio
 				break;
 			}
 
-			case GST_MESSAGE_STREAM_START:
-			{
-				static_cast<AudioPlayerBase*>(userData)->OnStreamStart();
-				break;
-			}
+//			case GST_EVENT_STREAM_START:
+//			{
+//				qDebug() << "st";
+//				static_cast<AudioPlayerBase*>(userData)->OnStreamStart();
+//				break;
+//			}
 
 			case GST_MESSAGE_ERROR:
 			{
